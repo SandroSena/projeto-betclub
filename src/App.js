915aa03c70d8bec33d42e6e9857e83b4021213cd
Pedro Sato <pedro.sato@gama.academy>
2020-05-25 19:50:30 -0300

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a></a>
         <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
