b101e7d40d4a86635b12dbc020e6a4e51b67babb
Sandro Sena <sandro340@hotmail.com>
2020-06-06 15:51:00 -0300

    Modal

---
+++ src/components/ModalEntrevistaJP.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const ModalEntrevistaJP = ({ onHide, isModalOpenJP }) => {
+  const ModalTitle = styled.h3`
+    font-size: 1.75rem;
+    padding: 1rem;
+  `;
+
+  return (
+    <Modal
+      show={isModalOpenJP}
+      onHide={onHide}
+      size='lg'
+      aria-labelledby='contained-modal-title-vcenter'
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Entrevista - Jovem Pan</Modal.Title>
+      </Modal.Header>
+      <ModalTitle>
+        <strong>COMO TRANSFORMAR A PAIXÃO POR FUTEBOL EM DINHEIRO</strong>
+      </ModalTitle>
+      <Modal.Body>
+        Você sabia que pode ganhar dinheiro assistindo a jogos de futebol? O
+        principal trader esportivo do Brasil e autor do livro Invi$ta em
+        Futebol, Juliano Fontes, falou para a rádio Jovem Pan sobre como se
+        tornar um investidor de sucesso.
+        <br />
+        <br />
+        Na entrevista, Fontes conta que existe diferença entre apostas
+        convencionais e o trading. No primeiro caso, você aposta em um time e,
+        se ele ganhar, você lucra; se não ganhar, você perde. Já no trading,
+        você pode comprar e vender ações ao longo do jogo, fazendo várias
+        operações e resgatando o valor que investiu. “Se aos 30 minutos do
+        segundo tempo de uma partida entre Brasil e Chile, que ainda permanece
+        no zero a zero, por exemplo, você percebe que o jogo está frio e nada
+        vai acontecer nos próximos dois ou três minutos, é possível comprar
+        ações acreditando no empate e, em seguida, vendê-las com um lucro de 1%
+        ou 2%”, explica Fontes.
+        <br />
+        <br />
+        Em outro cenário, o especialista mostra o Brasil ganhando de 1×0 contra
+        o Chile. “Se você investe no Brasil porque ele está jogando melhor e,
+        quase no fim da partida, o Chile começa a fazer jogadas brilhantes, tem
+        a opção de vender suas operações antes que esse gol seja concretizado”,
+        diz Fontes.
+        <br />
+        <br />
+        Esses e outros exemplos deixam claro que o trading esportivo é uma
+        atividade baseada em estudos e probabilidades, diferente das apostas.
+        Todos os investimentos são feitos por meio de uma bolsa esportiva,
+        chamada Betfair, que oferece maior fluidez para realizar as operações
+        com segurança.
+        <br />
+        <br />
+        Ainda de acordo com Juliano Fontes, qualquer pessoa pode participar e se
+        tornar um investidor, mas a recomendação é que o aprendiz goste muito de
+        futebol.
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalEntrevistaJP;
---
+++ src/components/ModalEntrevistaRG.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const ModalEntrevistaRG = ({ onHide, isModalOpenRG }) => {
+  const ModalTitle = styled.h3`
+    font-size: 1.75rem;
+    padding: 1rem;
+  `;
+
+  return (
+    <Modal
+      show={isModalOpenRG}
+      onHide={onHide}
+      size='lg'
+      aria-labelledby='contained-modal-title-vcenter'
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Entrevista - Rádio Globo</Modal.Title>
+      </Modal.Header>
+      <ModalTitle>
+        <strong>TORNE-SE UM INVESTIDOR</strong>
+      </ModalTitle>
+      <Modal.Body>
+        Em entrevista para a Rádio Globo, Juliano Fontes, idealizador do site
+        <a href='www.investimentofutebol.com'> www.investimentofutebol.com</a>,
+        explicou o que é o trader esportivo – modalidade de investimento que
+        mistura mercado de ações com aposta esportiva on-line. Mas o que isso
+        significa?
+        <br />
+        <br />
+        Significa que é possível tornar-se um investidor acompanhando partidas
+        de futebol, comprando e vendendo palpites dados ao longo da partida; ou,
+        ainda, por meio dos resultados fechados, que, na modalidade, são
+        chamados de investimentos. O aporte inicial é a partir de US$ 4.
+        <br />
+        <br />
+        Durante a entrevista, Fontes conta que o trader esportivo existe há 16
+        anos na Europa, onde chega a movimentar milhões de dólares entre times
+        pequenos. No Brasil, onde o investimento ainda é pouco conhecido,
+        partidas entre times grandes de campeonatos como o Brasileirão, Copa das
+        Américas ou Libertadores costumam movimentar entre US$ 300 e US$ 400
+        mil. “Há dois anos, o mercado brasileiro movimentava apenas metade
+        desses valores”, afirma Fontes.
+        <br />
+        <br />
+        O investidor salienta que, apesar de o trader esportivo derivar de
+        apostas, ele é considerado um investimento. “As leis brasileiras são
+        antigas e não contemplam a modalidade. Porém, o investimento é legal e
+        os ganhos são declarados normalmente no imposto de renda, sendo
+        transferidos para contas correntes nacionais, ou sacados via caixa
+        eletrônico”, explica Fontes aos ouvintes da Rádio Globo.
+        <br />
+        <br />
+        Para mais informações, tanto para novatos quanto para investidores que
+        desejam se aprimorar na modalidade, Juliano Fontes pode ser contatado
+        pelo site www.investimentofutebol.com.
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalEntrevistaRG;
